ca4e579726bccc0df836e0ca698496dbb181da4a
Ernest Gaise <ernestyawgaisie@gmail.com>
2024-12-05 13:42:06 -0500

    Render fxes

--- main.py
+++ main.py
@@ -108,4 +108,4 @@ def predict(data: InputData):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
